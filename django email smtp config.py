6b1f8c1e3777937cb9543dd417071cf54eba19b1
ahmed4end <35382418+ahmed4end@users.noreply.github.com>
2021-08-06 21:15:06 +0200

    sigh

--- django email smtp config.py
+++ django email smtp config.py
@@ -1,3 +1,7 @@
+# free mailing service url: app.sendgrid.com
+
+#EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend' # console.debug case
+
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 DEFAULT_FROM_EMAIL = 'email@mail.com'
 EMAIL_HOST = 'smtp.sendgrid.net'
