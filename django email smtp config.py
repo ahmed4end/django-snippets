1699c57f425a8c6b2c39c7b47ff47ce53737a46b
ahmed4end <35382418+ahmed4end@users.noreply.github.com>
2021-08-06 21:14:08 +0200

    Create django email smtp config.py

---
+++ django email smtp config.py
@@ -0,0 +1,7 @@
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+DEFAULT_FROM_EMAIL = 'email@mail.com'
+EMAIL_HOST = 'smtp.sendgrid.net'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'apikey'
+EMAIL_HOST_PASSWORD = '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'
